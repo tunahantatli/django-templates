2a9d35a1d493016ee0f169b78474655e3afd8161
Tunahan Tatli <tunahantatl@gmail.com>
2023-02-17 20:00:10 +0300

    some-changes-add

--- students/views.py
+++ students/views.py
@@ -7,6 +7,12 @@ def home(request):
         'description': 'This is a description. The code write by human',
         'number': 125637849,
         'list': ['hello', 'this', 'is', 'a', 'list', ['arif','fivefivefiveDotcom','gora']],
+        'e_list':[
+            {'name': 'selim', 'age':45},
+            {'name': 'ali', 'age':27},
+            {'name': 'veli', 'age':62},
+            {'name': 'salim', 'age':24}
+        ],
     }
     return render(request, 'students/home.html', context)
    # return HttpResponse('<h1>Hello</h1>')
