d658c47160171d1c4e1bf075b4c0870413115b85
Tunahan Tatli <tunahantatl@gmail.com>
2023-02-19 14:16:40 +0300

    form-start-dj-temp

--- students/models.py
+++ students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student( models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    department = models.CharField(max_length=20)
+    school_number = models.SmallIntegerField()
+    image = models.ImageField(upload_to='students', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name}  {self.last_name} {self.school_number}'
+    
